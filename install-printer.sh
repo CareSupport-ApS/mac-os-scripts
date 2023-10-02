68a5cade0fc35699aa96181c3b5407914be69c3e
Emil Møller Lind <lind@noque.dk>
2023-10-02 13:25:53 +0200

    sada

--- install-printer.sh
+++ install-printer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Variables (adjust these if needed)
-PRINTER_NAME="YourPrinterName"
+PRINTER_NAME="Nimbus_dfsdf"
+ESCAPED_PRINTER_NAME=$(echo "$PRINTER_NAME" | sed 's/ /\\ /g')
 DEVICE_URI="ipp://192.168.180.23/ipp/print" # something like ipp://Printer_IP/ipp/ or similar
 GITHUB_PPD_URL="https://raw.githubusercontent.com/CareSupport-ApS/mac-os-scripts/main/ppds/Nimbus%20-%20BW.ppd" # Replace with the actual URL
 PPD_DOWNLOAD_PATH="/tmp/YourPrinterName.ppd" # Temporary path for downloaded PPD #
 
 # Check if printer exists and delete it
-if lpstat -p "$PRINTER_NAME" &>/dev/null; then
-    sudo lpadmin -x "$PRINTER_NAME"
+if lpstat -p "$ESCAPED_PRINTER_NAME" &>/dev/null; then
+    sudo lpadmin -x "$ESCAPED_PRINTER_NAME"
 fi
 
 
@@ -16,7 +17,7 @@ fi
 curl -L "$GITHUB_PPD_URL" -o "$PPD_DOWNLOAD_PATH"
 
 # Use lpadmin to add or modify the printer
-sudo lpadmin -p "$PRINTER_NAME" -v "$DEVICE_URI" -P "$PPD_DOWNLOAD_PATH" -E
+sudo lpadmin -p "$ESCAPED_PRINTER_NAME" -v "$DEVICE_URI" -P "$PPD_DOWNLOAD_PATH" -E
 
 # Delete the downloaded PPD
 rm "$PPD_DOWNLOAD_PATH"
